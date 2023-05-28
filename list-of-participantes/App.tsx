9e8fb1c19c9e8f0216b08a9f8508473c7df91884
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-05-27 22:41:39 -0300

    Feat: Estilizando Elementos

--- list-of-participantes/App.tsx
+++ list-of-participantes/App.tsx
@@ -4,8 +4,30 @@ import { Text, View } from 'react-native'
 
 export default function App(){
   return(
-    <View>
-      <Text> React Native</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    }}
+    >
+      <Text style={{
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+      }}> 
+        Nome do Evento
+      </Text>
+
+      <Text
+        style={{
+          color: '#6B6B6B',
+          fontSize: 16,
+
+        }}
+      >
+        Sabado, 27 de Novembro de 2022
+      </Text>
     </View>
   )
 
