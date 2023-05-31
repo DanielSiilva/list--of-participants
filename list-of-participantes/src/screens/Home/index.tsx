5bab87c728adad4c250e56adc1a089e0f54cac4e
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-05-31 11:52:38 -0300

    Feat: Organizando o projeto

---
+++ list-of-participantes/src/screens/Home/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from 'react-native'
+
+import {styles} from './styles'
+
+
+
+export default function Home(){
+  return(
+    <View style={styles.container}>
+      <Text style={styles.eventName}> 
+        Nome do Evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Sabado, 27 de Novembro de 2022
+      </Text>
+    </View>
+  )
+
+}
